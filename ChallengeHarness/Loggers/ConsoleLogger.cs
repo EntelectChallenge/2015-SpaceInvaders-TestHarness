ad80d018844745526056d42440837ea0a9e45e40
Matt Van Der Westhuizen <mattpwest@gmail.com>
2015-04-24 16:18:21 +0200

    ConsoleLogger will now fall back to simple Console.WriteLine if the terminal is too small to support changing the cursor position. Using Console.SetWindowSize looks like a no-go on Linux, so this will have to do... Closes #4.

--- ChallengeHarness/Loggers/ConsoleLogger.cs
+++ ChallengeHarness/Loggers/ConsoleLogger.cs
@@ -6,6 +6,8 @@ namespace ChallengeHarness.Loggers
 {
     public class ConsoleLogger
     {
+		protected const string title = "Entelect 100K Challenge: Space Invaders";
+
         public ConsoleLogger()
         {
             Console.Clear();
@@ -13,24 +15,55 @@ namespace ChallengeHarness.Loggers
 
         public void Log(MatchRender rendered)
         {
-            var line = 0;
-            WriteToConsoleAndDebug(0, line++, "Entelect 100K Challenge: Space Invaders");
-
-            WriteToConsoleAndDebug(0, line, rendered.Map);
-            line += CalculateRenderedMapHeight(rendered.Map);
-
-            foreach (var move in rendered.Moves)
-            {
-                WriteToConsoleAndDebug(0, line++, move);
-            }
+			// 1 title line, 2 spacing lines and 2 move lines
+			if (IsConsoleTooSmall (CalculateRenderedMapHeight (rendered.Map) + 1 + 2 + 2)) {
+				LogWithScrolling (rendered);
+			} else {
+				LogWithoutScrolling (rendered);
+			}
         }
 
-        public void Log(MatchSummary summary)
-        {
-            var message = String.Format("Match result: Player {0} wins{1}Win reason: {2}", summary.Winner, Environment.NewLine, summary.WinReason);
-            Debug.WriteLine(message);
-            Console.WriteLine(message);
-        }
+		public void Log(MatchSummary summary)
+		{
+			var message = String.Format("Match result: Player {0} wins{1}Win reason: {2}", summary.Winner, Environment.NewLine, summary.WinReason);
+			Debug.WriteLine(message);
+			Console.WriteLine(message);
+		}
+
+		protected void LogWithScrolling (MatchRender rendered)
+		{
+			WriteToConsoleAndDebug (title);
+			WriteToConsoleAndDebug (rendered.Map);
+
+			Console.WriteLine ();
+			Console.WriteLine ();
+
+			foreach (var move in rendered.Moves)
+			{
+				WriteToConsoleAndDebug(move);
+			}
+		}
+
+		protected void LogWithoutScrolling (MatchRender rendered)
+		{
+			var line = 0;
+			
+			WriteToConsoleAndDebug (0, line++, title);
+
+			WriteToConsoleAndDebug(0, line, rendered.Map);
+			line += CalculateRenderedMapHeight(rendered.Map);
+
+			foreach (var move in rendered.Moves)
+			{
+				WriteToConsoleAndDebug(0, line++, move);
+			}
+		}
+
+		protected void WriteToConsoleAndDebug(string message) {
+			Debug.WriteLine(message);
+			Console.WriteLine (message);
+
+		}
 
         protected void WriteToConsoleAndDebug(int x, int y, string message)
         {
@@ -42,6 +75,11 @@ namespace ChallengeHarness.Loggers
             Console.SetCursorPosition(x, y);
             Console.WriteLine(message);
         }
+
+		protected bool IsConsoleTooSmall (int minHeight)
+		{
+			return Console.WindowHeight < minHeight;
+		}
 
         private int CalculateRenderedMapHeight(string mapView)
         {
