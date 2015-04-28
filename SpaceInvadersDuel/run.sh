546169ddf1081b523256634521fa2bdcdf543e62
Marius Kruger <amanic@gmail.com>
2015-04-28 15:03:28 +0200

    fix run.sh newline characters and give hints on installing dependencies

--- SpaceInvadersDuel/run.sh
+++ SpaceInvadersDuel/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# assumes mono and mono libraries are installed
+
+# on ubuntu you can install dependencies as follows:
+# sudo apt-get install mono-complete libnewtonsoft-json*
+
 mono SpaceInvadersDuel.exe $*
